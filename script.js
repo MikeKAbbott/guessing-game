787c4e4ca6a61e6cae276583af2040fd177256c7
mikekabbott <mikekabbott@gmail.com>
2020-08-24 12:28:57 -0400

    added difficulty

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /*
 TODO: 1. the guess is an actual word
-      4. new way to get words
-      5. add difficulty
 */
 
 /*
@@ -10,6 +8,7 @@ once teh start game button is pressed and end game function that triggers when t
 guesses the correct word, or gives up.
 */
 function Game() {
+    this.difficulty;
     this.init();
 }
 
@@ -18,6 +17,10 @@ Game.prototype.init = function () {
     this.addEventListeners();
 }
 
+//sets the games difficulty by getting the users radio button input
+Game.prototype.setDifficulty = function(){  
+    Game.difficulty = $("#difficulty-normal").prop("checked") ? "normal" : "hard";
+}
 //Adds the start and give up button to the event listener
 Game.prototype.addEventListeners = function () {
     $(document).on('click', '#give-up', this.gameOver);
@@ -34,9 +37,11 @@ Game.prototype.startGame = function () {
     for (node of this.parentNode.childNodes) {
         node.hidden = false;
     }
+    Game.setDifficulty();
+    Word.setWord();
     $('#restart-button').hide();
     $('#give-up').show();
-    this.hidden = true;
+    $(this).hide();
     stopWatch.startTime();
 }
 /* 
@@ -44,10 +49,10 @@ Function gameOver triggers if the player guesses the correct word or the player 
 Stops the clock, hides the guesses, and displays the results
 */
 Game.prototype.gameOver = function (guess) {
-    html = Word.winner ? `<span class="winner">Congrats! You guessed <b>${guess}</b>!</span>` : `<span>Looks like you gave up! The word was <b>${Word.todaysWord}</b></span>`;
+    html = Word.winner ? `<span class="winner">Congrats! You guessed <b>${guess}</b>!</span>` : `<span>Looks like you gave up! The word was <b>${Word.currentWord}</b></span>`;
     Word.guessList.hidden = true;
     stopWatch.stopTime();
-    for (node of $('#header').nextAll()) {
+    for (node of $('#clock').nextAll()) {
         $(node).hide();
     }
     $('#restart-button').show();
@@ -55,10 +60,12 @@ Game.prototype.gameOver = function (guess) {
     $('.grid').append(html);
 }
 
+/* 
+Lazy way to restart the game, reload the users window.
+*/
 Game.prototype.restartGame = function () {
     location.reload();
 }
-
 
 /*
 stopWatch class that handles the time of the game. Time is triggered once the game starts,
@@ -122,8 +129,9 @@ Word class that handles everything to do with the current word guessin game. Giv
 word to guess, gets the users guess, checks if the guess is right
 */
 function Word() {
+    //normal words from English words on TV and movies https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/TV/2006/1-1000
     this.normalWords = ['course','against','ready','daughter','work','friends','minute','though','supposed','honey','point','start','check','alone','matter','office','hospital','three','already','anyway','important','tomorrow','almost','later','found','trouble','excuse','hello','money','different','between','every','party','either','enough','year','house','story','crazy','mind','break','tonight','person','sister','pretty','trust','funny','gift','change','business','train','under','close','reason','today','beautiful','brother','since','bank','yourself','without','until','forget','anyone','promise','happy','bake','worry','school','afraid','cause','doctor','exactly','second','phone','look','feel','somebody','stuff','elephant','morning','heard','world','chance','call','watch','whatever','perfect','dinner','family','heart','least','answer','woman','bring','probably','question','stand','truth','problem','patch','pass','famous','true','power','cool','last','fish','remote','race','noon','wipe','grow','jumbo','learn','itself','chip','print','young','argue','clean','remove','flip','flew','replace','kangaroo','side','walk','gate','finger','target','judge','push','thought','wear','desert','relief','basic','bright','deal','father','machine','know','step','exercise','present','wing','lake','beach','ship','wait','fancy','eight','hall','rise','river','round','girl','winter','speed','long','oldest','lock','kiss','lava','garden','fight','hook','desk','test','serious','exit','branch','keyboard','naked','science','trade','quiet','home','prison','blue','window','whose','spot','hike','laptop','dark','create','quick','face','freeze','plug','menu','terrible','accept','door','touch','care','rescue','ignore','real','title','city','fast','season','town','picture','tower','zero','engine','lift','respect','time','mission','play','discover','nail','half','unusual','ball','tool','heavy','night','farm','firm','gone','help','easy','library','group','jungle','taste','large','imagine','normal','outside','paper','nose','long','queen','olive','doing','moon','hour','protect','hate','dead','double','nothing','restaurant','reach','note','tell','baby','future','tall','drop','speak','rule','pair','ride','ticket','game','hair','hurt','allow','oven','live','horse','bottle','rock','public','find','garage','green','heat','plan','mean','little','spend','nurse','practice','wish','uncle','core','stop','number','nest','magazine','pool','message','active','throw','pull','level','wrist','bubble','hold','movie','huge','ketchup','finish','pilot','teeth','flag','head','private','together','jewel','child','decide','listen','garbage','jealous','wide','straight','fall','joke','table','spread','laundry','deep','quit','save','worst','email','glass','scale','safe','path','camera','excellent','place','zone','luck','tank','sign','report','myself','knee','need','root','light','sure','page','life','space','magic','size','tape','food','wire','period','mistake','full','paid','horrible','special','hidden','rain','field','kick','ground','screen','risky','junk','juice','human','nobody','mall','bathroom','high','class','street','cold','metal','nervous','bike','internet','wind','lion','summer','president','empty','square','jersey','worm','popular','loud','online','something','photo','knot','mark','zebra','road','storm','grab','record','said','floor','theater','kitchen','action','equal','nice','dream','sound','fifth','comfy','talk','police','draw','bunch','idea','jerk','copy','success','team','favor','open','neat','whale','gold','free','mile','lying','meat','nine','wonderful','hero','quilt','info','radio','move','early','remember','understand','month','everyone','quarter','center','universe','name','zoom','inside','label','yell','jacket','nation','support','lunch','twice','hint','jiggle','boot','alive','build','date','room','fire','music','leader','rest','plant','connect','land','body','belong','trick','wild','quality','band','health','website','love','hand','okay','yeah','dozen','glove','give','thick','flow','project','tight','join','cost','trip','lower','magnet','parent','grade','angry','line','rich','owner','block','shut','neck','write','hotel','danger','impossible','illegal','show','come','want','truck','click','chocolate','none','done','bone','hope','share','cable','leaf','water','teacher','dust','orange','handle','unhappy','guess','past','frame','knob','winner','ugly','lesson','bear','gross','midnight','grass','middle','birthday','rose','useless','hole','drive','loop','color','sell','unfair','send','crash','knife','wrong','guest','strong','weather','kilometer','undo','catch','neighbor','stream','random','continue','return','begin','kitten','thin','pick','whole','useful','rush','mine','toilet','enter','wedding','wood','meet','stolen','hungry','card','fair','crowd','glow','ocean','peace','match','hill','welcome','across','drag','island','edge','great','unlock','feet','iron','wall','laser','fill','boat','weird','hard','happen','tiny','event','math','robot','recently','seven','tree','rough','secret','nature','short','mail','inch','raise','warm','gentle','glue','roll','search','regular','here','count','hunt','keep','week',]
-    //from vocabulary.com
+    //harder words from vocabulary.com
     this.hardWords = [
         "abject",
         "aberration",
@@ -428,7 +436,7 @@ function Word() {
     ];
 
     this.guesses = [];
-    this.todaysWord = this.todaysWord();
+    this.currentWord;
     this.guessList = document.getElementById('guesses');
     this.userInput = document.getElementById('user-input');
     this.counter = 0;
@@ -490,7 +498,7 @@ Word.prototype.guessWord = function (event) {
 Check if the guess is a winner 
 */
 Word.prototype.isWinner = function (guess) {
-    return guess.toLowerCase() == Word.todaysWord;
+    return guess.toLowerCase() == Word.currentWord;
 }
 
 /*
@@ -499,7 +507,7 @@ Give the player a hint with his guess
 Word.prototype.giveHint = function () {
     var lastItem = Word.guessList.lastChild;
     var guess = lastItem.innerHTML;
-    var compare = [guess, Word.todaysWord];
+    var compare = [guess, Word.currentWord];
     compare.sort()
     if ($('li').length > 1) {
         $(lastItem).prev().children().remove('span');
@@ -514,8 +522,10 @@ Word.prototype.giveHint = function () {
 /*
 Select a random word from the list
 */
-Word.prototype.todaysWord = function () {
-    return this.normalWords[Math.floor(Math.random() * this.normalWords.length)];
+Word.prototype.setWord = function () {
+    word =  Game.difficulty == "normal" ? this.normalWords[Math.floor(Math.random() * this.normalWords.length)] : this.hardWords[Math.floor(Math.random() * this.hardWords.length)]
+    Word.currentWord = word;
+    return word    
 }
 
 
@@ -526,5 +536,4 @@ $(window).on('load', function () {
     window.Game = new Game();
     window.Word = new Word();
     window.stopWatch = new stopWatch();
-    console.log(Word.todaysWord)
 })
