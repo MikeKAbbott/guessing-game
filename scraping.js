db99a2910f5aa8f5c8671f9102d6b2c7d71ea503
mikekabbott <mikekabbott@gmail.com>
2020-08-25 10:14:19 -0400

    deleted stupid files

--- scraping.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-axios.get('https://www.vocabulary.com/dictionary/').then(response =>{
-        const $ = cheerio.load(response.data);
-        return $('.dynamictext').children().first().text();
-    })
